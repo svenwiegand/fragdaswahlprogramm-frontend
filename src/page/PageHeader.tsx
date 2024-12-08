c227fcb42340b368976c8d0ce1e202ebafd3e7bb
Sven Wiegand <source@sven-wiegand.de>
2024-12-08 17:47:38 +0100

    Recycle classes.

--- src/page/PageHeader.tsx
+++ src/page/PageHeader.tsx
@@ -1,6 +1,7 @@
 import {css} from '@emotion/react'
 import {Link} from "../styled-compontents.tsx"
 import {FormattedMessage} from "react-intl"
+import styled from "@emotion/styled"
 
 const headerStyle = css`
     flex-shrink: 0;
@@ -17,22 +18,20 @@ const logoStyle = css`
     line-height: normal;
     color: #000;
 `
-const logoLightStyle = css`
-    ${logoStyle};
-    color: #888;
-`
 
 const spacerStyle = css`
     height: 1rem;
     flex: 1 0 0;
 `
 
+const LogoText = styled.span(logoStyle)
+
 export function PageHeader() {
     return (
         <div css={headerStyle}>
-            <span css={logoStyle}>frag</span>
-            <span css={logoLightStyle}>das</span>
-            <span css={logoStyle}>wahlprogramm</span>
+            <LogoText>frag</LogoText>
+            <LogoText css={css`color: #888`}>das</LogoText>
+            <LogoText>wahlprogramm</LogoText>
             <div aria-hidden={true} css={spacerStyle}></div>
             <Link href="#"><FormattedMessage id={"about"}/></Link>
         </div>
