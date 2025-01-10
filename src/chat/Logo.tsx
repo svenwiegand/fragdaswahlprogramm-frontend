cdeca1d9c37fae89e22abc0a59331b5eae6dea6a
Sven Wiegand <source@sven-wiegand.de>
2025-01-10 16:05:44 +0100

    Gradient-Logo

--- src/chat/Logo.tsx
+++ src/chat/Logo.tsx
@@ -1,5 +1,5 @@
 import {css} from "@emotion/react"
-import {color, dimensions, rempx} from "../style/styles.ts"
+import {dimensions, rempx} from "../style/styles.ts"
 
 const logoContainer = css`
     display: flex;
@@ -10,31 +10,49 @@ const logoContainer = css`
 
 const logoStyle = css`
     margin: ${rempx(48)} 0;
+    display: inline-block;
+    position: relative;
+    
     font-family: Outfit, sans-serif;
     font-size: ${rempx(48)};
     font-style: normal;
     font-weight: 600;
     line-height: normal;
-    color: #000;
-    
+
     @media (max-width: ${dimensions.mobileMaxWidth}) {
         margin: ${rempx(32)} 0;
         font-size: ${rempx(32)};
     }
 `
+const logoGradientStyle = css`
+    color: #000;
+    background: linear-gradient(270deg, #B431ED 0%, #006EE6 50%, #00A396 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+`
 const logoStyleLight = css`
-    ${logoStyle};
-    color: ${color.neutral.neutral600};
-}
+    color: rgba(0, 0, 0, 0);
+    background: none;
+    background-clip: initial;
+    
+    position: absolute;
+    top: 0;
+    left: 0;
+    
+    & > span {
+        color: rgba(255, 255, 255, 0.6);
+    }
 `
 
 
 export function Logo() {
     return (
         <div css={logoContainer}>
-            <span css={logoStyle}>frag</span>
-            <span css={logoStyleLight}>das</span>
-            <span css={logoStyle}>wahlprogramm</span>
+            <span css={logoStyle}>
+                <span css={logoGradientStyle}>frag<span>das</span>wahlprogramm</span>
+                <span css={logoStyleLight} aria-hidden={true}>frag<span>das</span>wahlprogramm</span>
+            </span>
         </div>
     )
 }
