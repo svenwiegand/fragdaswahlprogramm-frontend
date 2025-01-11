ec7f0f0f60893ed5282f2691ef005a6a0bcecd29
Sven Wiegand <source@sven-wiegand.de>
2025-01-11 17:55:24 +0100

    Program page offsets adjusted to optimized manifest files.

--- src/common/parties.ts
+++ src/common/parties.ts
@@ -15,7 +15,7 @@ export const parties: Record<Party, PartyProps> = {
         manifesto: {
             title: "Wahlprogramm der AfD",
             url: "https://www.bundestagswahl-bw.de/fileadmin/bundestagswahl-bw/2025/Wahlprogramme/AfD_Leitantrag-Bundestagswahlprogramm-2025.pdf",
-            pageOffset: 0,
+            pageOffset: -7,
         },
     },
     gruene: {
@@ -24,7 +24,7 @@ export const parties: Record<Party, PartyProps> = {
         manifesto: {
             title: "Wahlprogramm von Bündnis 90/Die Grünen",
             url: "https://cms.gruene.de/uploads/assets/20241216_BTW25_Programmentwurf_DINA4_digital.pdf",
-            pageOffset: 0,
+            pageOffset: -8,
         },
     },
     "cdu-csu": {
@@ -33,7 +33,7 @@ export const parties: Record<Party, PartyProps> = {
         manifesto: {
             title: "Wahlprogramm von CDU und CSU",
             url: "https://www.politikwechsel.cdu.de/wahlprogramm",
-            pageOffset: 2,
+            pageOffset: -9,
         },
     },
     fdp: {
@@ -42,7 +42,7 @@ export const parties: Record<Party, PartyProps> = {
         manifesto: {
             title: "Wahlprogramm der FDP",
             url: "https://www.fdp.de/sites/default/files/2024-12/fdp-wahlprogramm_2025.pdf",
-            pageOffset: 1,
+            pageOffset: -3,
         },
     },
     linke: {
@@ -60,7 +60,7 @@ export const parties: Record<Party, PartyProps> = {
         manifesto: {
             title: "Wahlprogramm der SPD",
             url: "https://www.spd.de/fileadmin/Dokumente/Beschluesse/Programm/Entwurf_Regierungsprogramm_2025.pdf",
-            pageOffset: 2,
+            pageOffset: -2,
         },
     },
     volt: {
