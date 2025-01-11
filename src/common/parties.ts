b855575af4ef287e3834a1eed5384bf16e5997cd
Sven Wiegand <source@sven-wiegand.de>
2025-01-11 18:19:58 +0100

    Documenting draft state of programs.

--- src/common/parties.ts
+++ src/common/parties.ts
@@ -6,6 +6,7 @@ type PartyProps = {
         title: string
         url: string
         pageOffset: number
+        draft: boolean
     }
 }
 export const parties: Record<Party, PartyProps> = {
@@ -16,6 +17,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm der AfD",
             url: "https://www.bundestagswahl-bw.de/fileadmin/bundestagswahl-bw/2025/Wahlprogramme/AfD_Leitantrag-Bundestagswahlprogramm-2025.pdf",
             pageOffset: 0,
+            draft: true,
         },
     },
     gruene: {
@@ -25,6 +27,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm von Bündnis 90/Die Grünen",
             url: "https://cms.gruene.de/uploads/assets/20241216_BTW25_Programmentwurf_DINA4_digital.pdf",
             pageOffset: 0,
+            draft: true,
         },
     },
     "cdu-csu": {
@@ -34,6 +37,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm von CDU und CSU",
             url: "https://www.politikwechsel.cdu.de/wahlprogramm",
             pageOffset: 2,
+            draft: false,
         },
     },
     fdp: {
@@ -43,6 +47,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm der FDP",
             url: "https://www.fdp.de/sites/default/files/2024-12/fdp-wahlprogramm_2025.pdf",
             pageOffset: 1,
+            draft: false,
         },
     },
     linke: {
@@ -52,6 +57,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm der Linken",
             url: "https://www.die-linke.de/fileadmin/1_Partei/parteitage/Au%C3%9Ferordentlicher_Parteitag_25/Wahlprogramm_Entwurf.pdf",
             pageOffset: 0,
+            draft: true,
         }
     },
     spd: {
@@ -61,6 +67,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm der SPD",
             url: "https://www.spd.de/fileadmin/Dokumente/Beschluesse/Programm/Entwurf_Regierungsprogramm_2025.pdf",
             pageOffset: 2,
+            draft: true,
         },
     },
     volt: {
@@ -70,6 +77,7 @@ export const parties: Record<Party, PartyProps> = {
             title: "Wahlprogramm von Volt",
             url: "https://voltdeutschland.org/storage/assets-btw25/volt-programm-bundestagswahl-2025.pdf",
             pageOffset: 0,
+            draft: false,
         },
     }
 }
